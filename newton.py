ee049936156682452529322f0be7b73bbd8a9b71
MortonC78483 <mortonc@stanford.edu>
2025-08-21 10:48:18 -0700

    multivariate addition

--- newton.py
+++ newton.py
@@ -1,5 +1,9 @@
 import numpy as np
-
+import numbers
+from autograd import hessian, grad
+import autograd.numpy as np
+from scipy.linalg import inv 
+import math
 
 def first_der(function, x, epsilon):
     """
@@ -10,10 +14,8 @@ def first_der(function, x, epsilon):
     @param epsilon: used for caculating derivatives
     @return: the forward differences-estimated first derivative
     """
-    num = function(x + epsilon) - function(x)
-    sign = np.sign(num)
-    return sign * np.exp(np.log(abs(num)) - np.log(epsilon))
-
+    num = function(x + epsilon) - function(x - epsilon)
+    return num/(2 * epsilon)
 
 def second_der(function, x, epsilon):
     """
@@ -25,13 +27,45 @@ def second_der(function, x, epsilon):
     @return: the forward differences-estimated second derivative
     """
     num = first_der(function, x + epsilon, epsilon) - first_der(
-        function, x, epsilon
+        function, x - epsilon, epsilon
     )  # numerator
-    sign = np.sign(num)
-    return sign * np.exp(np.log(abs(num)) - np.log(epsilon))
+    if num == 0:
+        return 1
+    else:
+        return num/(2 * epsilon)
 
+def single_variate_step(x_prev, x_cur, function, epsilon):
+    x_prev = x_cur
+    first = first_der(function, x_prev, epsilon)
+    second = second_der(function, x_prev, epsilon)
+    x_cur = (x_prev - first/second) 
+    return x_prev, x_cur
+    
+def multi_variate_step(x_prev, x_cur, function, epsilon):
+    # Compute Hessian using autograd
+    hess_f = hessian(function)
+    hess = hess_f(x_cur)
 
-def optimize(x_cur, function, epsilon=1e-5, diff=1e-9, stop_iter=5000):
+    # Compute gradient using autograd
+    grad_f = grad(function)
+    gradient = grad_f(x_cur)
+
+    # Newton-Raphson step
+    x_new = x_cur - np.matmul(inv(hess), gradient)
+
+    return np.copy(x_cur), np.copy(x_new)
+    
+    #x_prev = x_cur
+    #hess_f = jacobian(egrad(function))  # returns a function
+    #hess = hess_f(x_cur) 
+    #hess_f = hessian(function)
+    #hess = hess_f(x_prev)
+    #grad = first_der(function, x_prev, epsilon)
+    #x_cur = x_prev - np.matmul(inv(hess), grad)
+    #return x_prev, x_cur
+    
+    
+def optimize(x_cur, function, epsilon=1e-6, diff=1e-9, stop_iter=5000):
     """
     optimize runs Newton's method on a provided function, with a provided start value
 
@@ -42,25 +76,32 @@ def optimize(x_cur, function, epsilon=1e-5, diff=1e-9, stop_iter=5000):
     @param stop_iter: the maximum iterations before stopping
     @return: the extreme value found by the method
     """
-    x_prev = -float("inf")  # starting x_prev that won't violate tolerance
+    if not callable(function):
+        raise TypeError(f'`function` must be a function')
+        
+    x_prev = np.copy(x_cur) + 2 * diff # starting x_prev that won't violate tolerance
     iter = 0
-    while (abs(x_cur - x_prev) > diff) & (
+    
+    while (np.linalg.norm(x_cur - x_prev) > diff) & (
         iter < stop_iter
     ):  # while we haven't got under our tolerance
-        x_prev = x_cur
-        print(x_prev)
-        first = first_der(function, x_prev, epsilon)
-        sign_first = np.sign(first)
-        second = second_der(function, x_prev, epsilon)
-        sign_second = np.sign(second)
-        x_cur = (
-            x_prev
-            - (np.exp(np.log(abs(first)) - np.log(abs(second))))
-            * sign_first
-            * sign_second
-        )
+        if isinstance(x_cur, numbers.Number): # single variate
+            x_prev, x_cur = single_variate_step(x_prev, x_cur, function, epsilon)
+        else: # multivariate
+            print(x_prev, x_cur)
+            x_prev, x_cur = multi_variate_step(x_prev, x_cur, function, epsilon)
+            print(x_prev, x_cur)
         iter += 1
+
+        if np.linalg.norm(x_cur) > 1e7:
+            raise RuntimeError(f'function appears to diverge.')
+            
     if iter >= stop_iter:
         print("Maximum iterations exceeded! Oh no!")
         return None
     return x_cur
+
+    
+
+
+    
